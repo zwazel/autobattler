9d7e6b479f1d4326d671268a1c218f53a73a9989
Zwazel <zwazel@hotmail.com>
2022-01-24 22:58:08 +0100

    didnt get far

--- src/main/java/dev/zwazel/autobattler/classes/Utils/FindPath.java
+++ src/main/java/dev/zwazel/autobattler/classes/Utils/FindPath.java
@@ -1,9 +1,14 @@
 package dev.zwazel.autobattler.classes.Utils;
 
-public class FindPath {
-    public Vector findPath(Vector start, Vector end) {
-            
+import java.util.HashSet;
+import java.util.Map;
+import java.util.PriorityQueue;
 
+public class FindPath {
+    public Vector findPath(Vector start, Vector end, Grid grid) {
+        PriorityQueue<Vector> openList = new PriorityQueue<>();
+
+        HashSet<Vector> closedList = new HashSet<>();
         return start;
     }
 }

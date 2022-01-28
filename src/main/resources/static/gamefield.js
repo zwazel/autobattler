bcb3307cd8e5630f1a4e7d9b2938719ac61b7157
Muetzilla <muetz-github@gasbichler.ch>
2022-01-28 20:40:22 +0100

    set characters
    Took 55 minutes

--- src/main/resources/static/gamefield.js
+++ src/main/resources/static/gamefield.js
@@ -1,6 +1,30 @@
-function drawField(){
-    const rows = 5;
-    const columns = 5;
+let rows;
+let columns;
+const characterImageHtml = "<img src=\"../img/circle_01.png\" class=\"characterIcon\">";
 
-    for()
+function drawField(_rows, _columns) {
+    rows = _rows;
+    columns = _columns;
+    const gameBoad = document.getElementById("gameboard")
+    const htmlForRow = "<tr class=\"boardRow\">";
+    const htmlforCell = "<td></td>"
+    let gameBoardBuildingSting = "";
+
+    for (let i = 1; i <= rows; i++) {
+        gameBoardBuildingSting += htmlForRow;
+        for (let j = 1; j <= columns; j++) {
+            gameBoardBuildingSting += htmlforCell;
+        }
+        gameBoardBuildingSting += "</tr>";
+    }
+
+    gameBoad.innerHTML = gameBoardBuildingSting
+}
+
+function setRandomCharacter(){
+    const gameBoad = document.getElementById("gameboard")
+    let rowPos = Math.floor(Math.random() * rows + 1);
+    let columnPos = Math.floor(Math.random() * columns + 1);
+    let specificCell = gameBoad.rows[rowPos].cells[columnPos].innerHTML = characterImageHtml;
+
 }

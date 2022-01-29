38d1badab9ec0fd70609a6091f3fb1d525eb9786
Zwazel <zwazel@hotmail.com>
2022-01-29 14:38:48 +0100

    it kinda works!

--- src/main/resources/static/gamefield.js
+++ src/main/resources/static/gamefield.js
@@ -1,6 +1,6 @@
 let rows;
 let columns;
-const characterImageHtml = "characterIcon.html";
+const gameBoard = document.getElementById("gameboard")
 
 async function loadGridSize() {
     let response = await fetch("http://localhost:8080/getFightHistory");
@@ -9,9 +9,9 @@ async function loadGridSize() {
         // get the response body (the method explained below)
         let json = await response.json();
         let entity = JSON.parse(json.entity);
-        console.log(entity)
-        console.log(entity.gridSize)
         drawField(entity.gridSize.x, entity.gridSize.y)
+        initUnits(entity.unitsLeft)
+        // initUnits(entity.unitsRight)
     } else {
         alert("HTTP-Error: " + response.status);
     }
@@ -36,11 +36,42 @@ function drawField(_rows, _columns) {
     gameBoad.innerHTML = gameBoardBuildingSting
 }
 
+function getUnitIcon(unitId, unitName) {
+    let div = document.createElement("div");
+
+    let pUnitId = document.createElement("p")
+    pUnitId.id = "unitId-" + unitId
+    pUnitId.innerHTML = unitId
+
+    let pUnitName = document.createElement("p")
+    pUnitName.id = "unitName-" + unitName
+    pUnitName.innerHTML = unitName
+
+    let imgUnit = document.createElement("img")
+    imgUnit.classList.add("characterIconImage")
+    imgUnit.src = "img/circle_01.png"
+    imgUnit.alt = ""
+
+    div.append(pUnitId, pUnitName, imgUnit)
+
+    return div
+}
+
+function initUnits(units) {
+    console.log(units)
+    for (let i = 0; i < units.length; i++) {
+        let unit = units[i];
+        console.log(unit)
+        let unitPos = unit.position;
+        console.log(unitPos)
+        $(gameBoard.rows[unitPos.y].cells[unitPos.x]).append(getUnitIcon(unit.id, unit.name));
+    }
+}
+
 function setRandomCharacter() {
-    const gameBoad = document.getElementById("gameboard")
     let rowPos = Math.floor(Math.random() * rows + 1);
     let columnPos = Math.floor(Math.random() * columns + 1);
-    $(gameBoad.rows[rowPos].cells[columnPos]).load(characterImageHtml);
+    $(gameBoard.rows[rowPos].cells[columnPos]).load(characterImageHtml);
 }
 
 loadGridSize()

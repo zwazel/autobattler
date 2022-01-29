c26d459731db039c4df3680493941b6c59eade66
Restyled.io <commits@restyled.io>
2022-01-29 11:03:43 +0000

    Restyled by clang-format

--- src/main/resources/static/gamefield.js
+++ src/main/resources/static/gamefield.js
@@ -3,28 +3,27 @@ let columns;
 const characterImageHtml = "characterIcon.html";
 
 function drawField(_rows, _columns) {
-    rows = _rows;
-    columns = _columns;
-    const gameBoad = document.getElementById("gameboard")
-    const htmlForRow = "<tr class=\"boardRow\">";
-    const htmlforCell = "<td></td>"
-    let gameBoardBuildingSting = "";
+  rows = _rows;
+  columns = _columns;
+  const gameBoad = document.getElementById("gameboard")
+  const htmlForRow = "<tr class=\"boardRow\">";
+  const htmlforCell = "<td></td>"
+  let gameBoardBuildingSting = "";
 
-    for (let i = 1; i <= rows; i++) {
-        gameBoardBuildingSting += htmlForRow;
-        for (let j = 1; j <= columns; j++) {
-            gameBoardBuildingSting += htmlforCell;
-        }
-        gameBoardBuildingSting += "</tr>";
+  for (let i = 1; i <= rows; i++) {
+    gameBoardBuildingSting += htmlForRow;
+    for (let j = 1; j <= columns; j++) {
+      gameBoardBuildingSting += htmlforCell;
     }
+    gameBoardBuildingSting += "</tr>";
+  }
 
-    gameBoad.innerHTML = gameBoardBuildingSting
+  gameBoad.innerHTML = gameBoardBuildingSting
 }
 
-function setRandomCharacter(){
-    const gameBoad = document.getElementById("gameboard")
-    let rowPos = Math.floor(Math.random() * rows + 1);
-    let columnPos = Math.floor(Math.random() * columns + 1);
-    $(gameBoad.rows[rowPos].cells[columnPos]).load("characterIcon.html");
-
+function setRandomCharacter() {
+  const gameBoad = document.getElementById("gameboard")
+  let rowPos = Math.floor(Math.random() * rows + 1);
+  let columnPos = Math.floor(Math.random() * columns + 1);
+  $(gameBoad.rows[rowPos].cells[columnPos]).load("characterIcon.html");
 }

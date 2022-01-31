36e758d7b056eaf8cb839ce78ecd04da9660c4e4
Zwazel <zwazel@hotmail.com>
2022-01-31 10:54:06 +0100

    started work on db stuff!

--- src/main/java/dev/zwazel/autobattler/classes/Utils/User.java
+++ src/main/java/dev/zwazel/autobattler/classes/Utils/User.java
@@ -1,8 +1,19 @@
 package dev.zwazel.autobattler.classes.Utils;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
-    private final long id;
-    private final String username;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String username;
+
+    public User() {
+    }
 
     public User(long id, String username) {
         this.id = id;
---
+++ src/main/java/dev/zwazel/autobattler/classes/Utils/database/UserRepository.java
@@ -0,0 +1,8 @@
+package dev.zwazel.autobattler.classes.Utils.database;
+
+import dev.zwazel.autobattler.classes.Utils.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}

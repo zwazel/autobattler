f80fd54f43cf8b8357039531aa42d5a026a9a473
Zwazel <zwazel@hotmail.com>
2022-02-07 20:29:41 +0100

    AHAHHAHAHA DRAG AND DROP WORKS BITCH

--- src/main/resources/static/secured/js/pages/createFormation.js
+++ src/main/resources/static/secured/js/pages/createFormation.js
@@ -1,17 +1,19 @@
-console.log("guten tag 1")
+let copyCounter = 0;
 
 // get all the elements with the param draggable=true and add the event listener
 let elms = document.querySelectorAll("[draggable=true]");
 for (let i = 0; i < elms.length; i++) {
-    console.log("guten tag 3")
     let draggable = elms[i];
+    draggable.style.zIndex = "999";
     draggable.addEventListener("dragstart", drag);
 }
 
-const allGridCells = document.querySelectorAll('.unitCellWrapper');
+// find all elements with the class unitCellWrapper using jquery and add the event listener
+const allGridCells = $(".unitCellWrapper");
 for (let i = 0; i < allGridCells.length; i++) {
     allGridCells[i].addEventListener("dragover", allowDrop);
     allGridCells[i].addEventListener("drop", drop);
+    allGridCells[i].style.zIndex = "999";
 }
 
 function allowDrop(ev) {
@@ -19,13 +21,15 @@ function allowDrop(ev) {
 }
 
 function drag(ev) {
+    ev.dataTransfer.dropEffect = "copy";
     ev.dataTransfer.setData("text", ev.target.id);
-    console.log("guten tag 2")
 }
 
 function drop(ev) {
     ev.preventDefault();
     let data = ev.dataTransfer.getData("text");
-    ev.dataTransfer.dropEffect = "copy";
-    ev.target.appendChild(document.getElementById(data));
+    let original = document.getElementById(data);
+    let nodeCopy = original.cloneNode(true);
+    nodeCopy.id = original.id + "-copy-" + copyCounter++; /* We cannot use the same ID */
+    ev.target.appendChild(nodeCopy);
 }

50c53167489340d96c1192196139be2dc87281e4
Zwazel <zwazel@hotmail.com>
2022-02-16 11:45:16 +0100

    cleaned up code

--- src/main/java/dev/zwazel/autobattler/GUI.java
+++ src/main/java/dev/zwazel/autobattler/GUI.java
@@ -14,29 +14,30 @@ import java.text.DecimalFormat;
 import java.util.Iterator;
 
 public class GUI extends Canvas {
-    private static final DecimalFormat df = new DecimalFormat("0.00");
-    private final int scalar;
-    private final BattlerGen2 battlerGen2;
+    private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("0.00");
+    private final int SCALAR;
+    private final BattlerGen2 BATTLER_GEN2;
 
-    private final boolean differentiateSide = true;
-    private final boolean differentiateLastUnit = false;
-    private final boolean differentiateCurrentUnit = true;
-    private final boolean differentiateTarget = true;
-    private final boolean showLastPosition = true;
-    private final boolean drawPath = true;
+    private final boolean DIFFERENTIATE_SIDE = true;
+    private final boolean DIFFERENTIATE_LAST_UNIT = false;
+    private final boolean DIFFERENTIATE_CURRENT_UNIT = true;
+    private final boolean DIFFERENTIATE_TARGET = true;
+    private final boolean SHOW_LAST_POSITION = true;
+    private final boolean DRAW_PATH = true;
 
-    private final JFrame frame = new JFrame();
-    private final Label currentUnitLabel = new Label("Current Unit = ");
-    private final Label lastUnitLabel = new Label("Last Unit = ");
-    private final Label targetLabel = new Label("Target = ");
-    private final Label currentAction = new Label("Current Action = ");
+    private final JFrame FRAME = new JFrame();
+    private final Label CURRENT_UNIT_LABEL = new Label("Current Unit = ");
+    private final Label LAST_UNIT_LABEL = new Label("Last Unit = ");
+    private final Label TARGET_LABEL = new Label("Target = ");
+    private final Label CURRENT_ACTION = new Label("Current Action = ");
 
-    private final Color colorStart = Color.cyan;
-    private final Color colorCurrentUnit = Color.blue;
-    private final Color colorFriendly = Color.green;
-    private final Color colorEnemy = Color.pink;
-    private final Color colorTarget = Color.red;
-    private final Color colorLastUnit = Color.yellow;
+    private final Color COLOR_START = Color.cyan;
+    private final Color COLOR_CURRENT_UNIT = Color.blue;
+    private final Color COLOR_FRIENDLY = Color.green;
+    private final Color COLOR_ENEMY = Color.pink;
+    private final Color COLOR_TARGET = Color.red;
+    private final Color COLOR_LAST_UNIT = Color.yellow;
+    private final Color COLOR_GRID = Color.gray;
 
     private Iterator<Unit> unitIterator;
     private Node[] nodes = new Node[0];
@@ -49,71 +50,71 @@ public class GUI extends Canvas {
     private boolean canDoNext = true;
 
     public GUI(BattlerGen2 battlerGen2, int scalar) {
-        this.battlerGen2 = battlerGen2;
+        this.BATTLER_GEN2 = battlerGen2;
         Grid grid = battlerGen2.getGrid();
-        this.scalar = scalar;
-        this.unitIterator = battlerGen2.getUnits().listIterator();
+        this.SCALAR = scalar;
+        this.unitIterator = BATTLER_GEN2.getUnits().listIterator();
 
-        frame.setSize((grid.getWidth() * scalar) + scalar * 2, (grid.getHeight() * scalar) + scalar * 3);
+        FRAME.setSize((grid.getWidth() * SCALAR) + SCALAR * 2, (grid.getHeight() * SCALAR) + SCALAR * 3);
 
-        frame.add(this, BorderLayout.CENTER);
+        FRAME.add(this, BorderLayout.CENTER);
 
         Button nextButton = new Button("Next");
         nextButton.addActionListener(e -> {
             if (canDoNext) {
                 canDoNext = false;
 
-                if (unitIterator.hasNext() && !battlerGen2.isFightFinished()) {
+                if (unitIterator.hasNext() && !BATTLER_GEN2.isFightFinished()) {
                     lastUnit = currentUnit;
                     if (lastUnit != null) {
-                        this.lastUnitLabel.setText("Last Unit = " + lastUnit.getName() + " (" + lastUnit.getID() + "), at " + lastUnit.getGridPosition());
+                        this.LAST_UNIT_LABEL.setText("Last Unit = " + lastUnit.getName() + " (" + lastUnit.getID() + "), at " + lastUnit.getGridPosition());
                     }
                     currentUnit = unitIterator.next();
-                    this.currentUnitLabel.setText("Current Unit = " + currentUnit.getName() + " (" + currentUnit.getID() + ")");
-                    if (showLastPosition) {
+                    this.CURRENT_UNIT_LABEL.setText("Current Unit = " + currentUnit.getName() + " (" + currentUnit.getID() + ")");
+                    if (SHOW_LAST_POSITION) {
                         start = currentUnit.getGridPosition();
                     }
                     Vector positionBefore = currentUnit.getGridPosition();
-                    ActionHistory actionHistory = battlerGen2.doTurn(unitIterator, currentUnit, false);
-                    if (!showLastPosition) {
+                    ActionHistory actionHistory = BATTLER_GEN2.doTurn(unitIterator, currentUnit, false);
+                    if (!SHOW_LAST_POSITION) {
                         start = currentUnit.getGridPosition();
                     }
                     if (actionHistory.targets().length > 0) {
                         Unit target = actionHistory.targets()[0];
-                        this.currentAction.setText("Current Action = " + actionHistory.actionType());
+                        this.CURRENT_ACTION.setText("Current Action = " + actionHistory.actionType());
                         if (target != null) {
                             end = target.getGridPosition();
                             this.target = target;
                             FindPath findPath = new FindPath();
                             nodes = findPath.findPath(currentUnit.getGridPosition(), findPath.findClosestNearbyNode(grid, currentUnit.getGridPosition(), end), new GridGraph(grid));
-                            this.targetLabel.setText("Target = " + target.getName() + " (" + target.getID() + "), at " + end);
+                            this.TARGET_LABEL.setText("Target = " + target.getName() + " (" + target.getID() + "), at " + end);
                         } else {
                             end = null;
                             nodes = new Node[0];
-                            this.targetLabel.setText("Target = ");
+                            this.TARGET_LABEL.setText("Target = ");
                         }
                     } else {
                         end = null;
                         nodes = new Node[0];
-                        this.targetLabel.setText("Target = ");
-                        this.currentAction.setText("Current Action = ");
+                        this.TARGET_LABEL.setText("Target = ");
+                        this.CURRENT_ACTION.setText("Current Action = ");
                     }
 
                     if (!positionBefore.equals(currentUnit.getGridPosition())) {
-                        currentUnitLabel.setText(currentUnitLabel.getText() + ", moved from " + start + " to " + currentUnit.getGridPosition());
+                        CURRENT_UNIT_LABEL.setText(CURRENT_UNIT_LABEL.getText() + ", moved from " + start + " to " + currentUnit.getGridPosition());
                         currentUnitMoved = true;
                     } else {
-                        currentUnitLabel.setText(currentUnitLabel.getText() + ", stayed at " + currentUnit.getGridPosition() + ", ");
+                        CURRENT_UNIT_LABEL.setText(CURRENT_UNIT_LABEL.getText() + ", stayed at " + currentUnit.getGridPosition() + ", ");
                         currentUnitMoved = false;
                     }
                 }
 
-                if (!battlerGen2.isFightFinished() && !unitIterator.hasNext()) {
-                    unitIterator = battlerGen2.getUnits().listIterator();
+                if (!BATTLER_GEN2.isFightFinished() && !unitIterator.hasNext()) {
+                    unitIterator = BATTLER_GEN2.getUnits().listIterator();
                 }
 
-                if (battlerGen2.isFightFinished()) {
-                    this.currentAction.setText("Current Action = battle finished");
+                if (BATTLER_GEN2.isFightFinished()) {
+                    this.CURRENT_ACTION.setText("Current Action = battle finished");
                 }
 
                 repaint();
@@ -122,39 +123,43 @@ public class GUI extends Canvas {
                 System.out.println("Can't do next");
             }
         });
-        frame.add(nextButton, BorderLayout.SOUTH);
+        FRAME.add(nextButton, BorderLayout.SOUTH);
 
         // add the labels to a new panel underneath each other and add it to the frame as well on the north side
         JPanel labelPanel = new JPanel();
         labelPanel.setLayout(new BoxLayout(labelPanel, BoxLayout.Y_AXIS));
-        labelPanel.add(currentUnitLabel);
-        labelPanel.add(lastUnitLabel);
-        labelPanel.add(targetLabel);
-        labelPanel.add(currentAction);
-        frame.add(labelPanel, BorderLayout.NORTH);
+        labelPanel.add(CURRENT_UNIT_LABEL);
+        labelPanel.add(LAST_UNIT_LABEL);
+        labelPanel.add(TARGET_LABEL);
+        labelPanel.add(CURRENT_ACTION);
+        FRAME.add(labelPanel, BorderLayout.NORTH);
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        FRAME.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        FRAME.setVisible(true);
     }
 
     @Override
     public void paint(Graphics g) {
-        Grid grid = battlerGen2.getGrid();
+        Grid grid = BATTLER_GEN2.getGrid();
         // draw border of the map/grid
-        g.drawRect(0, 0, grid.getWidth() * scalar, grid.getHeight() * scalar);
+        g.drawRect(0, 0, grid.getWidth() * SCALAR, grid.getHeight() * SCALAR);
 
         // Draw start (where the unit initially was)
-        if (start != null && currentUnitMoved && showLastPosition) {
-            g.setColor(colorStart);
-            g.fillRect(start.getX() * scalar, start.getY() * scalar, scalar, scalar);
+        if (start != null && currentUnitMoved && SHOW_LAST_POSITION) {
+            g.setColor(COLOR_START);
+            g.fillRect(start.getX() * SCALAR, start.getY() * SCALAR, SCALAR, SCALAR);
         }
 
         // draw all the units
         Vector gridPositionNow = new Vector(0, 0);
         for (int row = 0; row < grid.getHeight(); row++) {
-            gridPositionNow.setY(row * scalar);
+            gridPositionNow.setY(row * SCALAR);
             for (int column = 0; column < grid.getWidth(); column++) {
-                gridPositionNow.setX(column * scalar);
+                gridPositionNow.setX(column * SCALAR);
+
+                // draw rect around the cell
+                g.setColor(COLOR_GRID);
+                g.fillRect(gridPositionNow.getX(), gridPositionNow.getY(), SCALAR, SCALAR);
 
                 GridCell gridCell = grid.getGridCells()[column][row];
                 if (gridCell.getCurrentObstacle() != null) {
@@ -162,31 +167,31 @@ public class GUI extends Canvas {
                     if (!obstacle.equals(currentUnit) && !obstacle.equals(lastUnit) && !obstacle.equals(target)) {
                         if (obstacle.getClass() == MyFirstUnit.class) {
                             MyFirstUnit unit = (MyFirstUnit) obstacle;
-                            if (differentiateSide) {
+                            if (DIFFERENTIATE_SIDE) {
                                 if (unit.getSide() == Side.FRIENDLY) {
                                     // friendlies
-                                    g.setColor(colorFriendly);
+                                    g.setColor(COLOR_FRIENDLY);
                                 } else {
                                     // enemies
-                                    g.setColor(colorEnemy);
+                                    g.setColor(COLOR_ENEMY);
                                 }
                             } else {
-                                g.setColor(colorFriendly);
+                                g.setColor(COLOR_FRIENDLY);
                             }
                         }
                     } else {
                         if (obstacle.equals(currentUnit)) {
                             // current unit
-                            differentiateFriendlyAndEnemy(g, currentUnit, differentiateCurrentUnit, colorCurrentUnit);
+                            differentiateFriendlyAndEnemy(g, currentUnit, DIFFERENTIATE_CURRENT_UNIT, COLOR_CURRENT_UNIT);
                         } else if (obstacle.equals(lastUnit) && !lastUnit.equals(target)) {
                             // last unit
-                            differentiateFriendlyAndEnemy(g, lastUnit, differentiateLastUnit, colorLastUnit);
+                            differentiateFriendlyAndEnemy(g, lastUnit, DIFFERENTIATE_LAST_UNIT, COLOR_LAST_UNIT);
                         } else {
                             // target unit
-                            differentiateFriendlyAndEnemy(g, target, differentiateTarget, colorTarget);
+                            differentiateFriendlyAndEnemy(g, target, DIFFERENTIATE_TARGET, COLOR_TARGET);
                         }
                     }
-                    g.fillRect(gridPositionNow.getX(), gridPositionNow.getY(), scalar, scalar);
+                    g.fillRect(gridPositionNow.getX(), gridPositionNow.getY(), SCALAR, SCALAR);
                     g.setColor(Color.BLACK);
                     if (obstacle.getClass() == MyFirstUnit.class) {
                         MyFirstUnit unit = (MyFirstUnit) obstacle;
@@ -197,24 +202,24 @@ public class GUI extends Canvas {
         }
 
         // draw the path
-        if (drawPath && currentUnitMoved) {
+        if (DRAW_PATH && currentUnitMoved) {
             Vector nodeBefore = null;
             for (Node n : nodes) {
                 if (nodeBefore == null) {
                     nodeBefore = currentUnit.getGridPosition();
                 }
                 g.setColor(Color.BLACK);
-                g.drawRect(n.getMyGridCell().getPosition().getX() * scalar, n.getMyGridCell().getPosition().getY() * scalar, scalar, scalar);
+                g.drawRect(n.getMyGridCell().getPosition().getX() * SCALAR, n.getMyGridCell().getPosition().getY() * SCALAR, SCALAR, SCALAR);
 
                 g.setColor(Color.RED);
-                g.drawLine(nodeBefore.getX() * scalar + (scalar / 2), nodeBefore.getY() * scalar + (scalar / 2), n.getMyGridCell().getPosition().getX() * scalar + (scalar / 2), n.getMyGridCell().getPosition().getY() * scalar + (scalar / 2));
-                g.drawString("" + df.format(n.getCost()), n.getMyGridCell().getPosition().getX() * scalar, n.getMyGridCell().getPosition().getY() * scalar);
+                g.drawLine(nodeBefore.getX() * SCALAR + (SCALAR / 2), nodeBefore.getY() * SCALAR + (SCALAR / 2), n.getMyGridCell().getPosition().getX() * SCALAR + (SCALAR / 2), n.getMyGridCell().getPosition().getY() * SCALAR + (SCALAR / 2));
+                g.drawString("" + DECIMAL_FORMAT.format(n.getCost()), n.getMyGridCell().getPosition().getX() * SCALAR, n.getMyGridCell().getPosition().getY() * SCALAR);
 
                 nodeBefore = n.getMyGridCell().getPosition();
             }
             if (nodeBefore != null) {
                 g.setColor(Color.RED);
-                g.drawLine(nodeBefore.getX() * scalar + (scalar / 2), nodeBefore.getY() * scalar + (scalar / 2), end.getX() * scalar + (scalar / 2), end.getY() * scalar + (scalar / 2));
+                g.drawLine(nodeBefore.getX() * SCALAR + (SCALAR / 2), nodeBefore.getY() * SCALAR + (SCALAR / 2), end.getX() * SCALAR + (SCALAR / 2), end.getY() * SCALAR + (SCALAR / 2));
             }
         }
     }
@@ -223,14 +228,14 @@ public class GUI extends Canvas {
         if (shouldDifferentiate) {
             g.setColor(colorIfDifferentiate);
         } else {
-            if (differentiateSide) {
+            if (DIFFERENTIATE_SIDE) {
                 if (unit.getSide() == Side.FRIENDLY) {
-                    g.setColor(colorFriendly);
+                    g.setColor(COLOR_FRIENDLY);
                 } else {
-                    g.setColor(colorEnemy);
+                    g.setColor(COLOR_ENEMY);
                 }
             } else {
-                g.setColor(colorFriendly);
+                g.setColor(COLOR_FRIENDLY);
             }
         }
     }

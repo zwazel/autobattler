855c250b989af5435108e3f43094d73b9abc9d5f
Zwazel <zwazel@hotmail.com>
2022-02-18 10:06:26 +0100

    scale movespeed + template/prototype constructor to quickly add new types!

--- src/main/java/dev/zwazel/autobattler/classes/enums/UnitTypes.java
+++ src/main/java/dev/zwazel/autobattler/classes/enums/UnitTypes.java
@@ -4,19 +4,12 @@ import dev.zwazel.autobattler.classes.abstractClasses.ScaleAttributeWithLevel;
 import dev.zwazel.autobattler.classes.exceptions.UnknownUnitType;
 
 public enum UnitTypes {
-    MY_FIRST_UNIT("First unit", 10, 100, 1, false, true, "MY_FIRST_UNIT",
+    MY_FIRST_UNIT("First unit", 10, 10, 1, false,
+            true, "MY_FIRST_UNIT", 1,
             ((health, level) -> ((int) (health + (health * ((level - 1) * 0.25))))),
-            ((energy, level) -> ((int) (energy + (energy * ((level - 1) * 0.25))))));
-
-    /**
-     * the default name of every unit of this type. If no custom name is set, this name will be used.
-     */
-    private final String defaultName;
-
-    /**
-     * defines if the user can change the name of the unit
-     */
-    private final boolean customNamesAllowed;
+            ((energy, level) -> ((int) (energy + (energy * ((level - 1) * 0.25))))),
+            ((moveSpeed, level) -> (moveSpeed))
+    );
 
     /**
      * the description of the unit
@@ -44,6 +37,21 @@ public enum UnitTypes {
     private final boolean canMoveDiagonally;
 
     /**
+     * the default name of every unit of this type. If no custom name is set, this name will be used.
+     */
+    private final String defaultName;
+
+    /**
+     * defines if the user can change the name of the unit
+     */
+    private final boolean customNamesAllowed;
+
+    /**
+     * how many slots in the formation this unit takes up
+     */
+    private final int slotSize;
+
+    /**
      * the health scale function, which takes the base health and the level and returns the scaled health
      */
     private final ScaleAttributeWithLevel scaleHealth;
@@ -53,7 +61,31 @@ public enum UnitTypes {
      */
     private final ScaleAttributeWithLevel scaleEnergy;
 
-    UnitTypes(String description, int baseHealth, int baseEnergy, int baseMoveSpeed, boolean canMoveDiagonally, boolean customNamesAllowed, String defaultName, ScaleAttributeWithLevel scaleHealth, ScaleAttributeWithLevel scaleEnergy) {
+    /**
+     * the move speed scale function, which takes the base move speed and the level and returns the scaled move speed
+     */
+    private final ScaleAttributeWithLevel scaleMoveSpeed;
+
+    /**
+     * default, placeholder constructor! to quickly create a new unit type, not suitable if more customizations are needed
+     *
+     * @param description the description of the unit
+     */
+    UnitTypes(String description) {
+        this.description = description;
+        this.baseHealth = 10;
+        this.baseEnergy = 10;
+        this.baseMoveSpeed = 1;
+        this.canMoveDiagonally = false;
+        this.customNamesAllowed = true;
+        this.defaultName = this.name();
+        this.slotSize = 1;
+        this.scaleHealth = ((health, level) -> health + ((level - 1) * 10)); // add 10 health per level
+        this.scaleEnergy = ((energy, level) -> energy + ((level - 1) * 10)); // add 10 energy per level
+        this.scaleMoveSpeed = ((moveSpeed, level) -> moveSpeed); // add 0 move speed per level, always keep it on 0!
+    }
+
+    UnitTypes(String description, int baseHealth, int baseEnergy, int baseMoveSpeed, boolean canMoveDiagonally, boolean customNamesAllowed, String defaultName, int slotSize, ScaleAttributeWithLevel scaleHealth, ScaleAttributeWithLevel scaleEnergy, ScaleAttributeWithLevel scaleMoveSpeed) {
         this.description = description;
         this.baseHealth = baseHealth;
         this.baseEnergy = baseEnergy;
@@ -61,8 +93,10 @@ public enum UnitTypes {
         this.canMoveDiagonally = canMoveDiagonally;
         this.customNamesAllowed = customNamesAllowed;
         this.defaultName = defaultName;
+        this.slotSize = slotSize;
         this.scaleHealth = scaleHealth;
         this.scaleEnergy = scaleEnergy;
+        this.scaleMoveSpeed = scaleMoveSpeed;
     }
 
     public static UnitTypes findUnitType(String unitTypeName) throws UnknownUnitType {
@@ -99,11 +133,19 @@ public enum UnitTypes {
         return customNamesAllowed;
     }
 
+    public int getSlotSize() {
+        return slotSize;
+    }
+
     public int scaleHealth(int level) {
         return scaleHealth.scale(this.baseHealth, level);
     }
 
     public int scaleEnergy(int level) {
         return scaleEnergy.scale(this.baseEnergy, level);
+    }
+
+    public int scaleMoveSpeed(int level) {
+        return scaleMoveSpeed.scale(this.baseMoveSpeed, level);
     }
 }

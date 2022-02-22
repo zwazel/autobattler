4f14db52d2fd3660150878ed41dea2395443fae1
Zwazel <zwazel@hotmail.com>
2022-02-22 11:56:04 +0100

    i already hate this my god. i'm trying to move from plain js/html to react

---
+++ src/main/java/dev/zwazel/autobattler/mvcController/HomeController.java
@@ -0,0 +1,14 @@
+package dev.zwazel.autobattler.mvcController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+
+}
---
+++ src/main/js/app.js
@@ -0,0 +1,15 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>HELLO</div>
+        )
+    }
+}

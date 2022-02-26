88ec08977f7016d0a3b5c58a804eab95b5c8a9be
Zwazel <zwazel@hotmail.com>
2022-02-26 14:35:48 +0100

    HTTP ONLY WORKS TOO!!!

--- src/main/java/dev/zwazel/autobattler/AutobattlerApplication.java
+++ src/main/java/dev/zwazel/autobattler/AutobattlerApplication.java
@@ -2,26 +2,10 @@ package dev.zwazel.autobattler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AutobattlerApplication {
     public static void main(String[] args) {
         SpringApplication.run(AutobattlerApplication.class, args);
     }
-
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//
-//                registry.addMapping("/**")
-//                        .allowedOrigins("http://localhost:3000")
-//                        .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
-//            }
-//        };
-//    }
 }

bce5c4b9b2dc639579d42818423350eaffee450d
Zwazel <zwazel@hotmail.com>
2022-02-26 19:55:54 +0100

    EHEYYYY WE'RE INITIALIZING THE DB WITH EVERYTHING THAT IS NEEDED!!!

---
+++ src/main/java/dev/zwazel/autobattler/springConfigs/Initializer.java
@@ -0,0 +1,39 @@
+package dev.zwazel.autobattler.springConfigs;
+
+import dev.zwazel.autobattler.classes.model.UserRole;
+import dev.zwazel.autobattler.classes.utils.EnumUserRole;
+import dev.zwazel.autobattler.classes.utils.database.repositories.UserRoleRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class Initializer implements CommandLineRunner {
+
+    private final UserRoleRepository userRoleRepository;
+
+    public Initializer(UserRoleRepository userRoleRepository) {
+        this.userRoleRepository = userRoleRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Initializing...");
+        System.out.println("Checking if DB has all needed roles...");
+
+        EnumUserRole[] userRoles = EnumUserRole.values();
+
+        for (EnumUserRole userRole : userRoles) {
+            Optional<UserRole> userRoleOptional = userRoleRepository.findByName(userRole);
+            if (userRoleOptional.isEmpty()) {
+                System.out.println(userRole + " not found, creating...");
+                userRoleRepository.save(new UserRole(userRole));
+            } else {
+                System.out.println(userRole + " found");
+            }
+        }
+
+        System.out.println("Done!");
+    }
+}
